7a36b3dc3bfc6d287a3c7bda10aacfaae8443f46
chrisp9 <chris_powell9@outlook.com>
2015-12-28 20:52:43 +0000

    mostly done for 1st pass

--- Hangman/Program.fs
+++ Hangman/Program.fs
@@ -1,7 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-type gameState =
+﻿type gameState =
    | Success
    | Fail
    | InProgress of int
@@ -10,7 +7,7 @@ let printState gameState =
    match gameState with 
    | Success -> printfn "You won!"
    | Fail -> printfn "You lost!"
-   | InProgress _ -> printfn "Incorrect guess"
+   | _ -> ()
 
 let initialGuess (word : char list) =
    word |> List.map (fun _ -> '-') |> Array.ofList |> System.String
@@ -23,29 +20,46 @@ let rec attemptGuess (word : char list, current : char list, guess : char) =
 
    match word.Tail.Length with
    | 0 -> unveiled
-   | _ -> unveiled + attemptGuess(word.Tail, word.Tail, guess)
+   | _ -> unveiled + attemptGuess(word.Tail, current.Tail, guess)
 
-let advanceState state =
-   match state with
-   | InProgress x when x < 9 -> gameState.InProgress(x + 1)
-   | InProgress x when x >= 9 -> gameState.Fail
-   | _ -> gameState.Success
+let advanceState state guessWasCorrect (currentWord : string) =
+   match state, guessWasCorrect, currentWord.Contains("-") with
+   | InProgress x, true, true when x < 8 -> gameState.InProgress(x)
+   | InProgress x, false, true when x < 8 -> gameState.InProgress(x+1)
+   | InProgress x, false, true when x >= 8 -> gameState.Fail
+   | _, true, false -> gameState.Success
+   | _, _, _ -> gameState.Fail
 
 let word() =
-   let dict = ["hello"; "world"]
-   List.sortBy (fun _ -> System.Random().Next(dict.Length)) dict |> List.head
+   let dict = ["hello"; "world"; "welcome"; "home";"antivivisectionist"]
+   dict.Item(System.Random().Next(dict.Length))
 
-let rec gameLoop(word, current) =
+let rec gameLoop(word, current, (state : gameState)) =
    let guess = System.Console.ReadKey().Key.ToString().ToLower().ToCharArray() |> List.ofArray |> List.head
    let newCurrent = attemptGuess(word, current, guess)
-   System.Console.WriteLine(newCurrent)
-   gameLoop(word, newCurrent.ToCharArray() |> List.ofArray)
+
+   let wasCorrect = current <> (newCurrent.ToCharArray() |> List.ofArray)
+   match wasCorrect with 
+   | true -> System.Console.WriteLine("Guessed correctly!")
+   | false -> System.Console.WriteLine("Incorrect guess!")
+
+   System.Console.WriteLine("\n" + newCurrent)
+   let newState = advanceState (state) (wasCorrect) newCurrent
+
+   printState(newState)
+
+   match newState with 
+   | InProgress _ -> gameLoop(word, newCurrent.ToCharArray() |> List.ofArray, newState)
+   | _ -> ()
+   ()
 
 [<EntryPoint>]
 let main argv = 
-   let theWord = word()
-   let initialGuess = initialGuess(Seq.toList (theWord.ToCharArray()))
-   System.Console.WriteLine (initialGuess)
-   gameLoop(theWord.ToCharArray() |> List.ofArray, initialGuess.ToCharArray() |> List.ofArray)
+   while true do
+      let theWord = word()
+      let initialGuess = initialGuess(Seq.toList (theWord.ToCharArray()))
+      System.Console.WriteLine (initialGuess)
+      gameLoop(theWord.ToCharArray() |> List.ofArray, initialGuess.ToCharArray() |> List.ofArray, gameState.InProgress(0))
+   
    printfn "%A" argv
    0 // return an integer exit code
