89457306f19b57a1bd9fe43edd681c83e10d89c8
chrisp9 <chris_powell9@outlook.com>
2015-12-28 19:00:51 +0000

    first commit

---
+++ Hangman/Program.fs
@@ -0,0 +1,54 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+type gameState =
+   | Success
+   | Fail
+   | InProgress of int
+
+let printState gameState =
+   match gameState with 
+   | Success -> printfn "You won!"
+   | Fail -> printfn "You lost!"
+   | InProgress _ -> printfn "Incorrect guess"
+
+let initialGuess (word : char list) =
+   word |> List.map (fun _ -> '-') |> Array.ofList |> System.String
+   
+let rec attemptGuess (word : char list, current : char list, guess : char) =
+   let t = word |> Seq.toList
+   let u = current |> Seq.toList
+
+   let unveiled = 
+      match t.Head with
+      | x when guess = t.Head -> t.Head.ToString()
+      | _ -> u.Head.ToString()
+
+   match t.Tail.Length with
+   | 0 -> unveiled
+   | _ -> unveiled + attemptGuess(t.Tail, u.Tail, guess)
+
+let advanceState state =
+   match state with
+   | InProgress x when x < 9 -> gameState.InProgress(x + 1)
+   | InProgress x when x >= 9 -> gameState.Fail
+   | _ -> gameState.Success
+
+let word() =
+   let dict = ["hello"; "world"]
+   List.sortBy (fun _ -> System.Random().Next(dict.Length)) dict |> List.head
+
+let rec gameLoop(word, current) =
+   let guess = System.Console.ReadKey().Key.ToString().ToLower().ToCharArray() |> List.ofArray |> List.head
+   let newCurrent = attemptGuess(word, current, guess)
+   System.Console.WriteLine(newCurrent)
+   gameLoop(word, newCurrent.ToCharArray() |> List.ofArray)
+
+[<EntryPoint>]
+let main argv = 
+   let theWord = word()
+   let initialGuess = initialGuess(Seq.toList (theWord.ToCharArray()))
+   System.Console.WriteLine (initialGuess)
+   gameLoop(theWord.ToCharArray() |> List.ofArray, initialGuess.ToCharArray() |> List.ofArray)
+   printfn "%A" argv
+   0 // return an integer exit code
